dfe385b7c50c9fb4405fd80e5d58c31f83a53992
blakehulett7 <blake.hulett7@gmail.com>
2024-12-22 11:39:40 -0600

    added top k in go!

--- go/main.go
+++ go/main.go
@@ -5,3 +5,24 @@ import "fmt"
 func main() {
 	fmt.Println("Christ is King!")
 }
+
+func top_k_frequent(nums []int, k int) []int {
+	counts := make(map[int]int)
+	for _, num := range nums {
+		counts[num]++
+	}
+	buckets := make([][]int, len(nums)+1)
+	for num, count := range counts {
+		buckets[count] = append(buckets[count], num)
+	}
+	result := []int{}
+	for i := len(nums); i >= 0; i-- {
+		for _, num := range buckets[i] {
+			result = append(result, num)
+			if len(result) == k {
+				return result
+			}
+		}
+	}
+	return result
+}
